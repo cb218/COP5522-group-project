1ec27b3177ed9b54045efa6d578282327b6aadc9
cb218 <cb218@students.uwf.edu>
2021-12-05 19:36:23 -0600

    Adding Odd-Even OMP

---
+++ odd_even_seq_OMP.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+
+void oe_sort_seq(int *numbers, int array_length, int Nthreads) {
+    int phase, idx, tmp;
+    #pragma omp parallel private (phase) num_threads(Nthreads)
+    for (phase = 0; phase < array_length; phase++) {
+        if (phase % 2 == 0) {
+            #pragma omp for
+            for (idx = 1; idx < array_length; idx += 2) {
+                if (numbers[idx - 1] > numbers[idx]) {
+                    tmp = numbers[idx - 1];
+                    numbers[idx - 1] = numbers[idx];
+                    numbers[idx] = tmp;
+                }
+            }
+        } else {
+            #pragma omp for
+            for (idx = 1; idx < array_length - 1; idx += 2) {
+                if (numbers[idx] > numbers[idx + 1]) {
+                    tmp = numbers[idx + 1];
+                    numbers[idx + 1] = numbers[idx];
+                    numbers[idx] = tmp;
+                }
+            }
+        }
+    }
+}
+
+void init_array(int *numbers, int array_length) {
+    for (int idx = 0; idx < array_length; idx++) {
+        numbers[idx] = rand() % array_length;
+    }
+}
+
+void print_array(int *numbers, int array_length) {
+    for (int idx = 0; idx < array_length; idx++) {
+        printf("%d ", numbers[idx]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    int array_length;
+    int num_cores;
+    int *numbers;
+
+    if (argc != 3) {
+        fprintf(stderr, "USAGE: %s Array_Length Num_cores\n", argv[0]);
+        exit(1);
+    }
+
+    array_length = atoi(argv[1]);
+    num_cores = atoi(argv[2]);
+
+    numbers = malloc(array_length * sizeof(int));
+    init_array(numbers, array_length);
+
+    struct timeval begin, end;
+
+    gettimeofday(&begin, 0);
+    oe_sort_seq(numbers, array_length, num_cores);
+    gettimeofday(&end, 0);
+
+    long begin_micro_seconds = 1.0e6 * begin.tv_sec + begin.tv_usec;
+    long end_micro_microseconds = 1.0e6 * end.tv_sec + end.tv_usec;
+    double elapsed_time = end_micro_microseconds - begin_micro_seconds;
+
+    printf("Array Length: %d\tNumber of Cores: %d\tTime measured: %g us\n",
+           array_length, num_cores, elapsed_time);
+
+    free(numbers);
+}
